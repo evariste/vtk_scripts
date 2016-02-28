e70f8e696e99a12bd429534648b4afa8914680e6
evariste <paul.aljabar@gmail.com>
2016-02-28 12:32:38 +0000

    New build to use.

--- sourceForVTKPython.sh
+++ sourceForVTKPython.sh
@@ -1,15 +1,17 @@
 # see http://www.itk.org/Wiki/VTK/Tutorials/PythonEnvironmentSetup
 
 
-#vtkBuildDir=/Users/paul/work/packages/vtk/build-5.6-py
-#vtkBuildDir=/Users/paul/work/packages/vtk/build-5.10-py
-vtkBuildDir="/Users/paulaljabar/work/packages/vtk/build-5.10.1-64"
+# vtkBuildDir=/Users/paul/work/packages/vtk/build-5.6-py
+# vtkBuildDir=/Users/paul/work/packages/vtk/build-5.10-py
+# vtkBuildDir="/Users/paulaljabar/work/packages/vtk/build-5.10.1-64"
+vtkBuildDir="/Users/paulaljabar/work/packages/vtk/build-6.3.0"
 
 export PATH=${vtkBuildDir}/bin:${PATH}
 
-export PYTHONPATH=${vtkBuildDir}/Wrapping/Python/:${vtkBuildDir}/bin:${PYTHONPATH}
+export PYTHONPATH=${vtkBuildDir}/Wrapping/Python/:${vtkBuildDir}/bin:${vtkBuildDir}/lib:${PYTHONPATH}
 
-export LD_LIBRARY_PATH=${vtkBuildDir}/bin
+export LD_LIBRARY_PATH=${vtkBuildDir}/lib
+export DYLD_LIBRARY_PATH=${vtkBuildDir}/lib
 
 # then run vtkpython
 
