9ba83e9271e5ab27798bf3bcb7bbe70b87a63eed
paul.aljabar <paul.aljabar@mirada-medical.com>
2017-07-03 15:00:18 +0100

    Update for Mirada machine.

--- sourceForVTKPython.sh
+++ sourceForVTKPython.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # see http://www.itk.org/Wiki/VTK/Tutorials/PythonEnvironmentSetup
 
 
@@ -5,13 +7,23 @@
 # vtkBuildDir=/Users/paul/work/packages/vtk/build-5.10-py
 # vtkBuildDir="/Users/paulaljabar/work/packages/vtk/build-5.10.1-64"
 vtkBuildDir="/Users/paulaljabar/work/packages/vtk/build-6.3.0"
+vtkBuildDir='/d/Users_D/paul.aljabar.MIRADA-MEDICAL/software/pa-builds/VTK-6.3.0-SRC/install'
 
-export PATH=${vtkBuildDir}/bin:${PATH}
+vtk_bin_dir="$vtkBuildDir/bin"
+vtk_lib_dir="$vtkBuildDir/lib"
+vtk_lib_py_dir="${vtk_lib_dir}/python2.7/site-packages"
 
-export PYTHONPATH=${vtkBuildDir}/Wrapping/Python/:${vtkBuildDir}/bin:${vtkBuildDir}/lib:${PYTHONPATH}
 
-export LD_LIBRARY_PATH=${vtkBuildDir}/lib
-export DYLD_LIBRARY_PATH=${vtkBuildDir}/lib
+allPaths="${vtkBuildDir}:${vtk_bin_dir}:${vtk_lib_dir}:${vtk_lib_py_dir}"
+export PATH="${allPaths}:${PATH}"
+
+# export PYTHONPATH=${vtkBuildDir}/Wrapping/Python/:${vtkBuildDir}/bin:${vtkBuildDir}/lib:${PYTHONPATH}
+export PYTHONPATH="${allPaths};${PYTHONPATH}"
+
+#export LD_LIBRARY_PATH=${vtkBuildDir}/lib
+export LD_LIBRARY_PATH="${vtk_lib_dir}:${vtk_bin_dir}:${PYTHONPATH}"
+
+#export DYLD_LIBRARY_PATH=${vtkBuildDir}/lib
 
 # then run vtkpython
 
